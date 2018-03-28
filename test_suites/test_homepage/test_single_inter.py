b5547f0aac68f3d59a34e9756bedc1b4e636fbee
U3\Administrator <lvjunjie1234@gmail.com>
2018-03-28 14:29:12 +0800

    update

--- test_suites/test_homepage/test_single_inter.py
+++ test_suites/test_homepage/test_single_inter.py
@@ -20,7 +20,7 @@ class Test_Single_Interface(unittest.TestCase):
     def test_new_poduct_list(self):
         """首页new_poduct_list"""
         homepage = HomePage()
-        homepage.login('15157163734', '147852')
+        homepage.login( '15157163734', 'a1478520B' )
         city_id = 112
         res = homepage.new_product_list(city_id)
         json = res.json
@@ -32,7 +32,7 @@ class Test_Single_Interface(unittest.TestCase):
     def test_local_advert_list(self):
         """首页local_advert_list"""
         homepage=HomePage()
-        homepage.login('15157163734', '147852')
+        homepage.login( '15157163734', 'a1478520B' )
         city_id = 112
         res = homepage.local_advert_list(city_id)
         json = res.json
@@ -44,7 +44,7 @@ class Test_Single_Interface(unittest.TestCase):
     def test_recommend_list(self):
         """首页recommend_list"""
         homepage = HomePage()
-        homepage.login('15157163734', '147852')
+        homepage.login( '15157163734', 'a1478520B' )
         city_id = 112
         res = homepage.recommend_list(city_id)
         json = res.json
@@ -56,7 +56,7 @@ class Test_Single_Interface(unittest.TestCase):
     def test_start_advertising(self):
         """首页start_advertising"""
         homepage = HomePage()
-        homepage.login('15157163734', '147852')
+        homepage.login( '15157163734', 'a1478520B' )
         res = homepage.start_advertising()
         json = res.json
         code = res.status_code
