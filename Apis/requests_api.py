c831de265b3363645325f5af692eb1777867c708
lvjunjie <lvjunjie84@hotmaol>
2018-04-18 13:19:55 +0800

    update sign

--- Apis/requests_api.py
+++ Apis/requests_api.py
@@ -9,14 +9,13 @@ class Hztz(object):
         self.header = {
             'User-Agent': 'Mozilla/5.0 (Linux; Android 8.0; BKL-AL20 Build/HUAWEIBKL-AL20; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/57.0.2987.132 MQQBrowser/6.2 TBS/043906 Mobile Safari/537.36 MicroMessenger/6.6.3.1260(0x26060339) NetType/WIFI Language/zh_CN',
             'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'Cookie': 'PHPSESSID=gio66aom4doc3oh5nirbbfbfgt'
+            'Cookie': 'PHPSESSID=bbtdn0k7kdg35cu6vp34milci0'
         }
 
     def sign(self, obj):
         """签到"""
         query = '/index/signin/addSave'
         headers = self.header
-        headers['a'] = 1
         body = {
             'obj': obj
         }
--- test_suites/test_hztz/test_sign.py
+++ test_suites/test_hztz/test_sign.py
@@ -1,21 +1,22 @@
 # coding=utf8
-from Apis.requests_test import Hztz
+from Apis.requests_api import Hztz
 import json
 import pytest
 
-class TestHztzSign( object ):
-    @pytest.fixture( autouse=True)
-    def before(self):
-        b = 'aaaaaaaaa'
-        return b
 
+@pytest.fixture(autouse=True)
+def before():
+    obj = 1
+    return obj
+
+
+class TestHztzSign(object):
     def test_hztz_sign_case(self, before):
         print before
         hztz = Hztz()
-        res = hztz.sign(1)
+        res = hztz.sign(before)
         # 获取当前玩家信息
         json = res.json()
         text = res.text
         print(text)
-        assert not json['hs_result']
-
+        assert "签" in text
